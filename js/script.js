df8c234ad02cd78cd99370e083205a6b5cdc8616
osvaldocastroo <osvaldocastro2704@gmail.com>
2024-08-25 12:40:07 -0300

    se agrega el fetch

--- js/script.js
+++ js/script.js
@@ -19,3 +19,10 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+// Realiza el fetch al archivo JSON y muestra los estudiantes
+fetch(DATA_URL)
+  .then(response => response.json())
+  .then(data => {
+    showData(data.students); // Llama a la función showData con los datos de los estudiantes
+  })
+  .catch(error => console.error('Error al cargar los datos:', error));
